1a65af9f876d083602a275db7b6373718db5e743
lokielse <lokielse@gmail.com>
2016-09-23 11:44:14 +0800

    -

--- tests/AopAppGatewayTest.php
+++ tests/AopAppGatewayTest.php
@@ -3,8 +3,6 @@
 namespace Omnipay\Alipay\Tests;
 
 use Omnipay\Alipay\AopAppGateway;
-use Omnipay\Alipay\AopWapGateway;
-use Omnipay\Alipay\Responses\CreateOrderResponse;
 use Omnipay\Alipay\Responses\TradeAppPayResponse;
 
 class AopAppGatewayTest extends AbstractGatewayTestCase
@@ -25,32 +23,28 @@ class AopAppGatewayTest extends AbstractGatewayTestCase
         $this->gateway->setAppId($this->appId);
         $this->gateway->setPrivateKey($this->appPrivateKey);
         $this->gateway->setEncryptKey($this->appEncryptKey);
-
         $this->gateway->setNotifyUrl('https://www.example.com/notify');
-
     }
 
 
     public function testPurchase()
     {
-        $str = '\U7528\U6237\U4e2d\U9014\U53d6\U6d88';
-
         /**
          * @var TradeAppPayResponse $response
          */
         $response = $this->gateway->purchase(
-            array (
-                'biz_content' => array (
+            [
+                'biz_content' => [
                     'subject'      => 'test',
                     'out_trade_no' => date('YmdHis') . mt_rand(1000, 9999),
-                    'total_amount' => 88.88,
+                    'total_amount' => '0.01',
                     'product_code' => 'QUICK_MSECURITY_PAY',
-                )
-            )
+                ]
+            ]
         )->send();
 
         $this->assertTrue($response->isSuccessful());
         $this->assertFalse($response->isRedirect());
-        die($response->getOrderString());
+        $this->assertNotEmpty($response->getOrderString());
     }
 }
