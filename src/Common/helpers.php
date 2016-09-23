67cc72aab8f36c795e4f3f8af0550d4e408db176
lokielse <lokielse@gmail.com>
2016-09-23 11:39:09 +0800

    Move

---
+++ src/Common/helpers.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Omnipay\Alipay\Common;
+
+/**
+ * Get an item from an array using "dot" notation.
+ *
+ * @param  array  $array
+ * @param  string $key
+ * @param  mixed  $default
+ *
+ * @return mixed
+ */
+use Closure;
+
+function array_get($array, $key, $default = null)
+{
+    if (is_null($key)) {
+        return $array;
+    }
+
+    if (isset($array[$key])) {
+        return $array[$key];
+    }
+
+    foreach (explode('.', $key) as $segment) {
+        if (! is_array($array) || ! array_key_exists($segment, $array)) {
+            return value($default);
+        }
+
+        $array = $array[$segment];
+    }
+
+    return $array;
+}
+
+function array_has($array, $key)
+{
+    if (empty($array) || is_null($key)) {
+        return false;
+    }
+
+    if (array_key_exists($key, $array)) {
+        return true;
+    }
+
+    foreach (explode('.', $key) as $segment) {
+        if (! is_array($array) || ! array_key_exists($segment, $array)) {
+            return false;
+        }
+
+        $array = $array[$segment];
+    }
+
+    return true;
+}
+
+/**
+ * Return the default value of the given value.
+ *
+ * @param  mixed $value
+ *
+ * @return mixed
+ */
+function value($value)
+{
+    return $value instanceof Closure ? $value() : $value;
+}
