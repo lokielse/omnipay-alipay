d19a9ccdd6cbb39fb8094abb490240f07be68886
QS <wic@qq.com>
2016-09-23 11:44:14 +0800

    -

--- tests/AopWapGatewayTest.php
+++ tests/AopWapGatewayTest.php
@@ -5,7 +5,7 @@ namespace Omnipay\Alipay\Tests;
 use Omnipay\Alipay\AopWapGateway;
 use Omnipay\Alipay\Responses\TradeWapPayResponse;
 
-class NewWapGatewayTest extends AbstractGatewayTestCase
+class AopWapGatewayTest extends AbstractGatewayTestCase
 {
 
     /**
@@ -22,10 +22,8 @@ class NewWapGatewayTest extends AbstractGatewayTestCase
         $this->gateway = new AopWapGateway($this->getHttpClient(), $this->getHttpRequest());
         $this->gateway->setAppId($this->appId);
         $this->gateway->setPrivateKey($this->appPrivateKey);
-        //$this->gateway->setEncryptKey($this->appEncryptKey);
-        //$this->gateway->setNotifyUrl('https://www.example.com/notify');
-        //$this->gateway->setReturnUrl('https://www.example.com/notify');
-        $this->gateway->setEndpoint('https://openapi.alipaydev.com/gateway.do');
+        $this->gateway->setNotifyUrl('https://www.example.com/notify');
+        $this->gateway->setReturnUrl('https://www.example.com/return');
     }
 
 
@@ -35,19 +33,18 @@ class NewWapGatewayTest extends AbstractGatewayTestCase
          * @var TradeWapPayResponse $response
          */
         $response = $this->gateway->purchase(
-            array (
-                'biz_content' => array (
+            [
+                'biz_content' => [
                     'out_trade_no' => date('YmdHis') . mt_rand(1000, 9999),
                     'total_amount' => 88.88,
                     'subject'      => 'test',
                     'product_code' => 'QUICK_MSECURITY_PAY',
-                )
-            )
+                ]
+            ]
         )->send();
 
         $this->assertTrue($response->isSuccessful());
-        $this->assertFalse($response->isRedirect());
-
-        die($response->getHtml());
+        $this->assertTrue($response->isRedirect());
+        $this->assertNotEmpty($response->getRedirectUrl());
     }
 }
