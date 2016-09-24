ae7956ea286cdf6f32ae29cba33d3f03393089f3
lokielse <lokielse@gmail.com>
2016-09-24 15:32:12 +0800

    Add

---
+++ src/Requests/AopCompletePurchaseRequest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Omnipay\Alipay\Requests;
+
+use Omnipay\Alipay\Responses\AopCompletePurchaseResponse;
+use Omnipay\Alipay\Responses\TradeQueryResponse;
+use function Omnipay\Alipay\Common\array_get;
+
+class AopCompletePurchaseRequest extends AbstractAopRequest
+{
+
+    /**
+     * Get the raw data array for this message. The format of this varies from gateway to
+     * gateway, but will usually be either an associative array, or a SimpleXMLElement.
+     *
+     * @return mixed
+     */
+    public function getData()
+    {
+        $this->validateParams();
+
+        return $this->getParams();
+    }
+
+
+    public function validateParams()
+    {
+        $this->validate('params');
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getParams()
+    {
+        return $this->getParameter('params');
+    }
+
+
+    /**
+     * Send the request with specified data
+     *
+     * @param  mixed $data The data to send
+     *
+     * @return AopCompletePurchaseResponse
+     */
+    public function sendData($data)
+    {
+        if (isset($data['result'])) {
+            $request = new AopVerifyAppPayReturnRequest($this->httpClient, $this->httpRequest);
+            $request->initialize($this->parameters->all());
+            $request->setResult($data['result']);
+            $request->setAlipayPublicKey($this->getAlipayPublicKey());
+            $data = $request->send()->getData();
+        } else {
+            $request = new AopNotifyRequest($this->httpClient, $this->httpRequest);
+            $request->initialize(['params' => $data]);
+            $request->setAlipayPublicKey($this->getAlipayPublicKey());
+            $data = $request->send()->getData();
+
+            if (! array_get($data, 'trade_status')) {
+                $tn = array_get($data, 'trade_no');
+
+                $request = new TradeQueryRequest($this->httpClient, $this->httpRequest);
+                $request->initialize($this->getParameters());
+                $request->setBizContent(['trade_no' => $tn]);
+                $request->setPrivateKey($this->getPrivateKey());
+
+                /**
+                 * @var TradeQueryResponse $response
+                 */
+                $response = $request->send();
+
+                $tradeStatus = $response->getAlipayResponse('trade_status');
+
+                $data['trade_status'] = $tradeStatus;
+            }
+        }
+
+        return $this->response = new AopCompletePurchaseResponse($this, $data);
+    }
+
+
+    /**
+     * @param $value
+     *
+     * @return $this
+     */
+    public function setParams($value)
+    {
+        return $this->setParameter('params', $value);
+    }
+}
---
+++ src/Responses/AbstractResponse.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Omnipay\Alipay\Responses;
+
+use Omnipay\Common\Message\AbstractResponse as Response;
+use function Omnipay\Alipay\Common\array_get;
+
+abstract class AbstractResponse extends Response
+{
+
+    public function data($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return $this->data;
+        } else {
+            return array_get($this->data, $key, $default);
+        }
+    }
+}
